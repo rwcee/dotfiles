7e15ea1a70b8aebaab5bea334424dd5a5e45a9fb
Rob Campbell <iamrwc@gmail.com>
2012-12-12 12:11:10 -0500

    Update domain_add.sh
    Removed the need for a blank VirtualHost file to exist.

--- domain_add.sh
+++ domain_add.sh
@@ -4,11 +4,20 @@ echo "Welcome to the Domain Provisioning script."
 if [ $(id -u) -eq 0 ]; then
 	echo -n "What is the name of your domain to provision? (example.com) : "
 	read domain
-	mkdir /var/www/html/$domain
-	cp /etc/apache2/sites-available/blank /etc/apache2/sites-available/$domain	
-	vi /etc/apache2/sites-available/$domain
+	mkdir /var/www/html/$domain	
+	touch /etc/apache2/sites-available/$domain
+	touch /var/www/log/$domain-access.log
+	touch /var/www/log/$domain-error.log
+	echo "<VirtualHost *:80>" >> /etc/apache2/sites-available/$domain
+	echo "	ServerName $domain" >> /etc/apache2/sites-available/$domain
+	echo "	ServerAlias www.$domain" >> /etc/apache2/sites-available/$domain
+	echo "	DocumentRoot /var/www/html/$domain" >> /etc/apache2/sites-available/$domain
+	echo "	CustomLog /var/www/log/$domain-access.log combined" >> /etc/apache2/sites-available/$domain
+	echo "	ErrorLog /var/www/log/$domain-error.log" >> /etc/apache2/sites-available/$domain
+	echo "</VirtualHost>" >> /etc/apache2/sites-available/$domain
 	ln -s /etc/apache2/sites-available/$domain /etc/apache2/sites-enabled/$domain
-	echo "Your domain has been provisioned."
+	echo "Your domain has been provisioned. Please verify in /var/www/html"
+	echo "and /etc/apache2/sites-enabled/."
 	exit 1
 else
 	echo "Only root may provision domains."
